598d5f76c620c081b5e067f9bf4df7f2c99cdcd5
yuvisingh121 <157522108+yuvisingh121@users.noreply.github.com>
2026-04-27 11:55:39 +0200

    update file StudyRoom.sql

--- src/StudyRoom.sql
+++ src/StudyRoom.sql
@@ -2,96 +2,112 @@ CREATE DATABASE if not exists StudyRoom;
 USE StudyRoom;
 
 CREATE TABLE if not exists Studente (
-    Id INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(255) NOT NULL,
-    Cognome VARCHAR(255) NOT NULL,
-    Username VARCHAR(255) NOT NULL UNIQUE,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    cognome VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
-    Password VARCHAR(255) NOT NULL
+    passcode VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE if not exists Amministratore (
-    Id INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(255) NOT NULL,
-    Cognome VARCHAR(255) NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    cognome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    Password VARCHAR(255) NOT NULL
+    passcode VARCHAR(255) NOT NULL
 );
+
 
 CREATE TABLE if not exists Corso_di_laurea (
     codice INT PRIMARY KEY AUTO_INCREMENT,
-    Nome_corso_di_laurea VARCHAR(255) NOT NULL
+    nome_Cdl VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE if not exists Insegnamento (
     codice INT PRIMARY KEY AUTO_INCREMENT,
-    Nome_insegnamento VARCHAR(255) NOT NULL,
-    Codice_corso_di_laurea INT NOT NULL,
-    FOREIGN KEY (Codice_corso_di_laurea) REFERENCES Corso_di_laurea(codice)
+    nome_Insegnamento VARCHAR(255) NOT NULL,
+    codice_Cdl INT NOT NULL,
+
+    FOREIGN KEY (codice_Cdl) REFERENCES Corso_di_laurea(codice)
 );
 
 CREATE TABLE if not exists Appunto (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    Titolo VARCHAR(255) NOT NULL,
-    tag ENUM('appunto','riassunto','esercizi') NOT NULL,
-    Codice_insegnamento INT NOT NULL,
-    Id_studente INT NOT NULL,
-    Url_file VARCHAR(255) NOT NULL,
-    Dimensione_file FLOAT NOT NULL,
-    FOREIGN KEY (Codice_insegnamento) REFERENCES Insegnamento(codice),
-    FOREIGN KEY (Id_studente) REFERENCES Studente(Id)
+    titolo VARCHAR(255) NOT NULL,
+    tag ENUM('Note','Riassunto','Esercizi') NOT NULL,
+    codice_Insegnamento INT NOT NULL,
+    id_Studente INT NOT NULL,
+    url_File VARCHAR(255) NOT NULL,
+    dimensione_File FLOAT NOT NULL,
+
+    FOREIGN KEY (codice_Insegnamento) REFERENCES Insegnamento(codice),
+    FOREIGN KEY (id_Studente) REFERENCES Studente(id)
 );
 
-CREATE TABLE if not exists esame (
+CREATE TABLE if not exists Esame (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    Titolo VARCHAR(255) NOT NULL,
-    Codice_insegnamento INT NOT NULL,
-    Id_studente INT NOT NULL,
-    Url_file VARCHAR(255) NOT NULL,
-    Dimensione_file FLOAT NOT NULL,
-    FOREIGN KEY (Codice_insegnamento) REFERENCES Insegnamento(codice),
-    FOREIGN KEY (Id_studente) REFERENCES Studente(Id)
+    titolo VARCHAR(255) NOT NULL,
+    codice_Insegnamento INT NOT NULL,
+    id_Studente INT NOT NULL,
+    url_File VARCHAR(255) NOT NULL,
+    dimensione_File FLOAT NOT NULL,
+
+    FOREIGN KEY (codice_Insegnamento) REFERENCES Insegnamento(codice),
+    FOREIGN KEY (id_Studente) REFERENCES Studente(id)
 )
+
 
 CREATE TABLE if not exists Recensione (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    Testo VARCHAR(255) NOT NULL,
-    Voto SMALLINT NOT NULL,
-    Id_appunto INT NULLABLE,
-    Id_esame INT NULLABLE,
-    Id_studente INT NOT NULL,
-    FOREIGN KEY (Id_appunto) REFERENCES Appunto(id),
-    FOREIGN KEY (Id_esame) REFERENCES esame(id),
-    FOREIGN KEY (Id_studente) REFERENCES Studente(Id)
+    commento VARCHAR(255) NOT NULL,
+    voto SMALLINT NOT NULL,
+    id_Appunto INT NULLABLE,
+    id_Esame INT NULLABLE,
+    id_Studente INT NOT NULL,
+
+    FOREIGN KEY (id_Appunto) REFERENCES Appunto(id),
+    FOREIGN KEY (id_Esame) REFERENCES esame(id),
+    FOREIGN KEY (id_Studente) REFERENCES Studente(id)
 );
 
 CREATE TABLE if not exists Download (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    Id_appunto INT NULLABLE,
-    Id_esame INT NULLABLE,
-    Id_studente INT NOT NULL,
-    FOREIGN KEY (Id_appunto) REFERENCES Appunto(id),
-    FOREIGN KEY (Id_esame) REFERENCES esame(id),
-    FOREIGN KEY (Id_studente) REFERENCES Studente(Id)
+    id_Appunto INT NULLABLE,
+    id_Esame INT NULLABLE,
+    id_Studente INT NOT NULL,
+
+    FOREIGN KEY (id_Appunto) REFERENCES Appunto(id),
+    FOREIGN KEY (id_Esame) REFERENCES esame(id),
+    FOREIGN KEY (id_Studente) REFERENCES Studente(id)
 );
 
 CREATE TABLE if not exists Segnalazione (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    Testo VARCHAR(255) NOT NULL,
-    Id_appunto INT NULLABLE,
-    Id_esame INT NULLABLE,
-    Id_studente INT NOT NULL,
-    FOREIGN KEY (Id_appunto) REFERENCES Appunto(id),
-    FOREIGN KEY (Id_esame) REFERENCES esame(id),
-    FOREIGN KEY (Id_studente) REFERENCES Studente(Id)
+    motivo VARCHAR(255) NOT NULL,
+    id_Appunto INT NULLABLE,
+    id_Esame INT NULLABLE,
+    id_Studente_Segnalatore INT NOT NULL,
+    id_Studente_Segnalato INT NOT NULL,
+    id_Amministratore INT NOT NULL,
+
+    FOREIGN KEY (idd_appunto) REFERENCES Appunto(id),
+    FOREIGN KEY (id_esame) REFERENCES esame(id),
+    FOREIGN KEY (id_Studente_Segnalatore) REFERENCES Studente(id),
+    FOREIGN KEY (id_Studente_Segnalato) REFERENCES Studente(id),
+    FOREIGN KEY(id_Amministratore) REFERENCES Amministratore(id)
+    
 );
+
 
 CREATE TABLE if not exists Preferito (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    Id_appunto INT NULLABLE,
-    Id_esame INT NULLABLE,
-    Id_studente INT NOT NULL,
-    FOREIGN KEY (Id_appunto) REFERENCES Appunto(id),
-    FOREIGN KEY (Id_esame) REFERENCES esame(id),
-    FOREIGN KEY (Id_studente) REFERENCES Studente(Id)
+    id_Appunto INT NULLABLE,
+    id_Esame INT NULLABLE,
+    id_Studente INT NOT NULL,
+
+    FOREIGN KEY (id_Appunto) REFERENCES Appunto(id),
+    FOREIGN KEY (id_Esame) REFERENCES esame(id),
+    FOREIGN KEY (id_Studente) REFERENCES Studente(id)
+
 );
