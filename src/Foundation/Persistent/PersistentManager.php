e1a7711f8a468c9204efe8b7d34b1d85671b2157
Lorenzo-64bit <lorenzocoppolone@gmail.com>
2026-05-05 11:22:45 +0200

    Aggiunta la classe session finta, creata la prima query in foundation per la restituzione dei materiali, nella ricerca con titolo

--- src/Foundation/Persistent/PersistentManager.php
+++ src/Foundation/Persistent/PersistentManager.php
@@ -1,19 +1,29 @@
 <?php
-/*
+namespace Foundation\Persistent;
+use doctrine\ORM\EntityManagerInterface;
+use Dom\Entity;
+
 class PersistentManager {
 
-    
-    public function cercaMateriale( String $titolo) : Array{
-        $tuttiIMateriali = [
-            new Appunto("M001", "Fondamenti di Informatica", "Rossi",    2020),
-            new Appunto("M002", "Programmazione Web",        "Bianchi",  2021),
-            new Esame("M003", "Basi di Dati",              "Verdi",    2019),
-            new Esame("M004", "Programmazione OO",         "Rossi",    2022),
-            new Esame("M005", "Reti di Calcolatori",       "Neri",     2020),
-            new Esame("M006", "Algoritmi e Strutture Dati","Bianchi",  2021),
-        ];
-        return $tuttiIMateriali;
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->em = $entityManager;
     }
-    
+
+
+    public function CercaMaterialePerTitolo(String $Titolo) {
+        // Logica per cercare i materiali nel database
+        $qb = $this->em->createQueryBuilder();
+        $qb->select('m.Id, m.Titolo, m.Insegnamento, m.Tipologia, m.CorsoDiLaurea, m.Tag, m.File')
+            ->from(Entity::class, 'Materiale', 'm')
+            ->join('m', 'Insegnamento', 'I', 'WITH', 'm.insegnamento_codice = I.CodiceInsegnamento')
+            ->join('I', 'CorsoDiLaurea', 'C', 'WITH', 'I.corsoDiLaurea_codice = C.codiceCorso')
+            ->where($qb->expr()->like('m.Titolo', ':Titolo'))
+            ->setParameter('Titolo', $Titolo);
+        $query = $qb->getQuery();
+        $materiali = $query->getArrayResult();    
+        return $materiali;
+    }
+   
 }
-*/
