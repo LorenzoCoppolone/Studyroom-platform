782a20434bea3b44ff8c2fcd19b292e2549cd1a2
yuvisingh121 <157522108+yuvisingh121@users.noreply.github.com>
2026-05-14 10:24:01 +0200

    session

--- src/Foundation/Session.php
+++ src/Foundation/Session.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(__DIR__ . '/../../../../config/config.php');
+require_once(__DIR__ . '/../../../../config/doctrine-bootstrap.php');
 /**
  * class to access to the $_SESSION superglobal array, you Must use this class instead of using directly the array
  */
